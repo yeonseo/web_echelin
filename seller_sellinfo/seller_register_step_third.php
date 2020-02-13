c9afec0b08492e0dc47bc68439c89c9ad8ee0c63
j222su <patrickstar0910@gmail.com>
2020-02-14 00:30:07 +0900

    스크롤시 하단 버튼 div 고정 테스트

---
+++ seller_sellinfo/seller_register_step_third.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <section>
+      <img src="./image/cheese.png" alt="">
+      <span id="header_span">3단계 : 손님을 맞이할 준비를 해주세요. </span>
+
+      <div class="">
+        <form class="" action="index.html" method="post">
+
+        <ul>
+          <li>최대 예약 가능 인원을 적어주세요.
+          </br>
+            <input type="text" name="" value="">
+          </li>
+
+          <li>예약 가능한 시간을 설정해주세요.
+          </br>
+            <input type="time" name="" value="">&nbsp-
+            <input type="time" name="" value="">
+          </li>
+
+          <li>예약 가능한 기간을 설정해주세요.
+          </br>
+            <input type="date" name="" value="">&nbsp-
+            <input type="date" name="" value="">
+          </li>
+        </ul>
+
+        <input type="button" name="" value="계속" onclick="location.href='./seller_register_complete.php'">
+        </form>
+      </div>
+    </section>
+  </body>
+</html>
