a677f5de7eaa091eff2bbe3b48c66df7addabc15
yeonseo <yulga_nara@naver.com>
2020-02-13 22:14:56 +0900

    main_test.html 파일을 php 파일로 분할하고 DB 연동하도록 수정함

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Echelin 에브리원 미슐랭</title>
+
+    <!-- CSS, JS 파일 링크 시, -->
+    <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/css/common.css">
+    <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/css/main.css">
+    <link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/css/main_test.css">
+
+    <!-- <link rel="stylesheet" href="./css/main.css"> -->
+    <script src="./js/vendor/modernizr.custom.min.js"></script>
+    <script src="./js/vendor/jquery-1.10.2.min.js"></script>
+    <script src="./js/vendor/jquery-ui-1.10.3.custom.min.js"></script>
+</head>
+
+<body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/echelin/common/database/create_table.php"; ?>
+    <header>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/echelin/header.php"; ?>
+    </header>
+    <section>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/echelin/main.php"; ?>
+    </section>
+    <footer>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/echelin/footer.php"; ?>
+    </footer>
+</body>
+
+</html>
