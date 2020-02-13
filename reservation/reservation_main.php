6285b159b8e3bca295830ecac71670cf5fc1dbaa
hadongwoon <hadong302@naver.com>
2020-02-13 14:56:02 +0900

    client reservation first form

---
+++ reservation/reservation_main.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./reservation.css">
+    <title>메인</title>
+  </head>
+  <script type="text/javascript">
+
+  </script>
+  <body>
+    <header>
+      <div class="header_div">
+        <span><h3> 메인 헤더 </h3></span><span> 헤더 </span><span> 헤더 </span><span> 내용 확인 </span>
+      </div>
+    </header>
+    <section>
+      <div class="main_section_div">
+
+          <img src="./img/topmain.png" alt="">
+            <img src="./img/topmain1.png" alt="">
+            <img src="./img/topmain2.png" alt="">
+            <img src="./img/topmain3.png" alt="">
+            <img src="./img/topmain3_1.png" alt="">
+            <img src="./img/topmain4.png" alt="">
+            <img src="./img/topmain5.png" alt="">
+        <a class="btnnext" href="./reservation_first.php">다음단계</a>
+        </div>
+    </section>
+    <footer>
+      <div class="footer_div" >
+        <h1>푸터</h1>
+      </div>
+    </footer>
+  </body>
+</html>
---
+++ reservation/reservation_pay.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="./reservation.css">
+    <title>예약 4단계</title>
+  </head>
+  <script type="text/javascript">
+
+  </script>
+  <body>
+    <header>
+      <div class="header_div">
+        <span><h3> 예약 </h3></span><span> 4/4 </span><span> 단계 </span><span> 결제 </span>
+      </div>
+    </header>
+    <section>
+      <div class="main_section_div">
+        <div class="date_check_div">
+
+
+            <h1>카카오페이</h1>
+            <h1>네이버페이</h1>
+            <h1>무통장입금페이</h1>
+        </div>
+        <div class="notice_check_div">
+          결제 완료시 데이더베이스 인서트
+        <a class="btnnext">결제 완료</a>
+        </div>
+    </section>
+    <footer>
+      <div class="footer_div" >
+        <h1>푸터</h1>
+      </div>
+    </footer>
+  </body>
+</html>
