f7e28a7e95649cdfab5240060a17422a0591194f
j222su <patrickstar0910@gmail.com>
2020-02-14 11:11:37 +0900

    판매자 등록 1단계 파일 경로 오류 수정

--- seller/seller_register_step_first.php
+++ seller/seller_register_step_first.php
@@ -8,33 +8,69 @@
   </head>
   <body>
     <section>
-      <img src="./image/cheese.png" alt="">
-      <span id="header_span">1단계 : 기본 사항을 입력해주세요. </span>
+      <div class="div_step">
+        <img class="img_echelin_logo" src="../seller/image/cheese.png" alt="">
+        <span class="span_step_info">1단계 : 기본 사항을 입력해주세요.</span>
+      </div>
 
-      <div class="">
-        <span>김지수</span>
-        <span id="hello_span">님 안녕하세요!</span>
-        <span id="greet_span">가게 등록을 시작해볼까요?</span>
+      <div class="div_step_charge1"></div>
 
-        <div id="first_step_div">
+      <div class="div_step_charge2"></div>
+
+      <div class="div_step_charge3"></div>
+
+      <div class="div_step_charge4"></div>
+
+      <div class="div_register_shape">
+        <div class="div_register_inner_shape">
+          <span class="span_user_name">김지수</span>
+          <span class="hello_span">님 안녕하세요!</span>
+          <span class="greet_span">가게 등록을 시작해볼까요?</span>
+
           <span>1단계</span>
           <span class="blind"></span>
-        </div>
-      </div>
 
-
-      <div class="">
-        <form class="" action="index.html" method="post">
-          <ul>
-            <li>등록하시려는 가게 이름을 적어주세요.</li>
-              <input type="text" name="" value="">
-            <li>사업자등록번호를 등록해주세요</li>
-              <input type="text" name="" value="">
-              <button type="button" name="button">찾기</button>
-          </ul>
-            <input type="button" name="" value="계속" onclick="location.href='./seller_register_step_first2.php'">
-        </form>
-      </div>
+          <div class="div_form">
+            <form class="" action="index.html" method="post">
+              <ul>
+                <li>등록하시려는 가게 이름을 적어주세요.</li>
+                  <input type="text" name="" value="">
+                <li>사업자등록번호를 등록해주세요</li>
+                  <input type="text" name="" value="">
+                  <button type="button" name="button">찾기</button>
+                  <p>ddsdf
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfsdf </br>
+                  sdfszzdf </br>
+                  sdfsdf </br>
+                </p>
+              </ul>
+              <div class="justtest">
+                <div class="justtest2">
+                  <input type="button" name="" value="계속" onclick="location.href='./seller_register_step_first2.php'">
+                </div>
+              </div>s
+            </form>
+          </div>
+        </div> <!-- div_register_inner_shape -->
+    </div> <!--div_register_shape-->
     </section>
   </body>
 </html>
