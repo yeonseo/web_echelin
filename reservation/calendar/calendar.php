84f34bbfb5aeb602ad5a429fecaf5c67a926f8f4
hadongwoon <hadong302@naver.com>
2020-02-16 07:40:01 +0900

    캘린더 오류수정

---
+++ reservation/calendar/calendar.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="./css/calendar.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+  <title>Vanilla Coding | Bootcamp Prep</title>
+  <script type="text/javascript" src="./js/calendar.js" defer>
+
+  </script>
+</head>
+<body>
+    <div class="content-wrap">
+      <div class="content-left">
+        <div class="main-wrap">
+          <div id="main-day" class="main-day"></div>
+          <div id="main-date" class="main-date"></div>
+        </div>
+        <div class="todo-wrap">
+          <div class="todo-title">Todo List</div>
+          <div class="input-wrap">
+            <input type="text" placeholder="please write here!!" id="input-box" class="input-box">
+            <button type="button" id="input-data" class="input-data">INPUT</button>
+            <div id="input-list" class="input-list"></div>
+          </div>
+        </div>
+      </div>
+      <div class="content-right">
+        <table id="calendar" align="center">
+          <thead>
+            <tr class="btn-wrap clearfix">
+              <td>
+                <label id="prev">
+                    &#60;
+                </label>
+              </td>
+              <td align="center" id="current-year-month" colspan="5"></td>
+              <td>
+                <label id="next">
+                    &#62;
+                </label>
+              </td>
+            </tr>
+            <tr>
+                <td class = "sun" align="center">Sun</td>
+                <td align="center">Mon</td>
+                <td align="center">Tue</td>
+                <td align="center">Wed</td>
+                <td align="center">Thu</td>
+                <td align="center">Fri</td>
+                <td class= "sat" align="center">Sat</td>
+              </tr>
+          </thead>
+          <tbody id="calendar-body" class="calendar-body"></tbody>
+        </table>
+      </div>
+    </div>
+</body>
+</html>
