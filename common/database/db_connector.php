d5a6716c7f1734b239ab346b1d2caa18fe68412e
yeonseo <yulga_nara@naver.com>
2020-02-16 00:02:34 +0900

    php-mysql 객체 형으로 변경 완료

--- common/database/db_connector.php
+++ common/database/db_connector.php
@@ -4,36 +4,47 @@ date_default_timezone_set("Asia/Seoul");
 $servername = "localhost";
 $dbusername = "root";
 $password = "123456";
+$dbname = "echelin";
 $dbflag = "NO";
 
-$con = mysqli_connect($servername, $dbusername, $password);
-if (!$con) {
-    die("Connection failed: " . mysqli_connect_error());
+
+//mysqli object-oriented로 적용함
+$con = new mysqli($servername, $dbusername, $password);
+if ($con->connect_error) {
+    die("DB Connection Filed: " . mysqli_connect_error());
 }
 
 $sql = "show databases";
-$result = mysqli_query($con, $sql) or die('Error: ' . mysqli_error($con));
+$result = $con->query($sql);
+if ($result === FALSE) {
+    die('Show Databases Error: ' . mysqli_error($con));
+}
 
-while ($row = mysqli_fetch_row($result)) {
-    if ($row[0] === 'echelin') {
-        $dbflag = "OK";
-        break;
+//echelin Database가 없을 경우 생성함
+if ($row = $result->num_rows > 0) {
+    while ($row = $result->fetch_array()) {
+        if ($row[0] === $dbname) {
+            $dbflag = "OK";
+            break;
+        }
     }
 }
 
 if ($dbflag === "NO") {
-    $sql = "create database echelin";
+    $sql = "create database " . $dbname;
 
-    if (mysqli_query($con, $sql)) {
-        echo "<script>alert('echelin 디비 생성되었습니다.');</script> ";
+    if ($con->query($sql)) {
+        echo "<script>alert('$dbname 디비 생성되었습니다.');</script> ";
     } else {
         echo "실패원인" . mysqli_error($con);
     }
 }
 
-//2. 데이타 베이스 선택 use kdhong_db
-$dbconn = mysqli_select_db($con, "echelin") or die('Error: ' . mysqli_error($con));
-
+$sql = "use " . $dbname;
+$dbconn = $con->query($sql);
+if ($dbconn === FALSE) {
+    die('DB Database Connect Error: ' . mysqli_error($con));
+}
 
 function test_input($data)
 {
