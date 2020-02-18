c992839eaeeb27b347ef7687aaf2d5ea5c079ba0
j222su <patrickstar0910@gmail.com>
2020-02-18 22:48:09 +0900

    판매자 등록 2단계 메뉴 추가시 테이블 생성 완료(기존 input값 삭제)

--- seller/seller_register_step_two.php
+++ seller/seller_register_step_two.php
@@ -6,88 +6,19 @@
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script src="./js/seller_register.js"></script>
     <link rel="stylesheet" href="../common/css/seller_register_step.css">
-    <link rel="stylesheet" href="../common/css/user_seller.css?ver=2">
+    <link rel="stylesheet" href="../common/css/user_seller.css?ver=3">
   </head>
   <body class="body_margin">
     <section>
-      <script type="text/javascript">
-      function previewImage(targetObj, View_area) {
-var preview = document.getElementById(View_area); //div id
-var ua = window.navigator.userAgent;
-
-//ie일때(IE8 이하에서만 작동)
-if (ua.indexOf("MSIE") > -1) {
-  targetObj.select();
-  try {
-    var src = document.selection.createRange().text; // get file full path(IE9, IE10에서 사용 불가)
-    var ie_preview_error = document.getElementById("ie_preview_error_" + View_area);
-
-
-    if (ie_preview_error) {
-      preview.removeChild(ie_preview_error); //error가 있으면 delete
-    }
-
-    var img = document.getElementById(View_area); //이미지가 뿌려질 곳
-
-    //이미지 로딩, sizingMethod는 div에 맞춰서 사이즈를 자동조절 하는 역할
-    img.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+src+"', sizingMethod='scale')";
-  } catch (e) {
-    if (!document.getElementById("ie_preview_error_" + View_area)) {
-      var info = document.createElement("<p>");
-      info.id = "ie_preview_error_" + View_area;
-      info.innerHTML = e.name;
-      preview.insertBefore(info, null);
-    }
-  }
-//ie가 아닐때(크롬, 사파리, FF)
-} else {
-  var files = targetObj.files;
-  for ( var i = 0; i < files.length; i++) {
-    var file = files[i];
-    var imageType = /image.*/; //이미지 파일일경우만.. 뿌려준다.
-    if (!file.type.match(imageType))
-      continue;
-    var prevImg = document.getElementById("prev_" + View_area); //이전에 미리보기가 있다면 삭제
-    if (prevImg) {
-      preview.removeChild(prevImg);
-    }
-    var img = document.createElement("img");
-    img.id = "prev_" + View_area;
-    img.classList.add("obj");
-    img.file = file;
-    img.style.width = '100px';
-    img.style.height = '100px';
-    preview.appendChild(img);
-    if (window.FileReader) { // FireFox, Chrome, Opera 확인.
-      var reader = new FileReader();
-      reader.onloadend = (function(aImg) {
-        return function(e) {
-          aImg.src = e.target.result;
-        };
-      })(img);
-      reader.readAsDataURL(file);
-    } else { // safari is not supported FileReader
-      //alert('not supported FileReader');
-      if (!document.getElementById("sfr_preview_error_"
-          + View_area)) {
-        var info = document.createElement("p");
-        info.id = "sfr_preview_error_" + View_area;
-        info.innerHTML = "not supported FileReader";
-        preview.insertBefore(info, null);
-      }
-    }
-  }
-}
-}
-      </script>
+      </div>
       <div class="div_step">
         <img class="img_echelin_logo" src="../seller/image/cheese.png" alt="">
         <span class="span_step_info">2단계 : 상세 정보를 입력해주세요</span>
       </div>
 
       <progress value="75" max="100">0%</progress>
-      <div id='View_area' style='position:relative; width: 100px; height: 100px; color: black; border: 0px solid black; dispaly: inline; '>
-
+      <div  class="div_outside">
+        dhd?
       </div>
       <div class="div_register_shape">
         <div class="div_register_inner_shape">
@@ -99,28 +30,24 @@ if (ua.indexOf("MSIE") > -1) {
                   <textarea name="name" rows="8" cols="74"></textarea>
                   </br></br>
 
-                  <li>식당 메뉴</li>
-                  <div id="div_add_input">
-                    <input input class="input_info" type="text" name="menu" value="">
-                    <button id="button_add" class="button_circle_add" type="button" name="button">+</button>
-                  </div>
-                  </br></br>
+                  <li id="li_menu">식당 메뉴</li>
+                  <button id="button_add" class="button_circle_add" type="button">+</button>
+                  <br>
+                  <br>
+                  <table class="table_seller_menu" border="1">
+                      <tbody>
+                          <tr>
+                            <th>메뉴</th>
+                            <th>가격</th>
+                            <th>사진</th>
+                          </tr>
 
-
-                  <table id="table_menu" class="table_seller_menu">
-                    <tr>
-                      <th>메뉴</th>
-                      <th>가격</th>
-                      <th>사진</th>
-                    </tr>
-
-                    <tr>
-                      <td></td>
-                      <td><input type="number" name="" value=""></td>
-                      <td><input type="file" name="profile_pt" id="profile_pt" onchange="previewImage(this,'View_area')"></td>
-                    </tr>
-
-                    <tbody></tbody>
+                          <tr name="tr_menu">
+                            <td><input type="text" name="" placeholder="메뉴이름"></td>
+                            <td><input type="number" name="" placeholder="가격"></td>
+                            <td><input type="file" name="" value=""></td>
+                          </tr>
+                      </tbody>
                   </table>
                  </br></br>
 
