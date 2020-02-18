a3d8d1ece53021727c3da34a8fb83313b9087f0e
yeonseo <yulga_nara@naver.com>
2020-02-18 15:32:48 +0900

    헤더부분의 link&script, 왼쪽 사이드 메뉴 구조화 및 구글 차트 테스트 작업

---
+++ common/common_link_script.php
@@ -0,0 +1,9 @@
+<!-- 웹폰트  -->
+<link rel="stylesheet" type="text/css" href="https://cdn.rawgit.com/moonspam/NanumSquare/master/nanumsquare.css">
+
+<!-- 아이콘 폰트  https://fontawesome.com/  -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/v4-shims.css">
+
+<!-- 구글차트  -->
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
