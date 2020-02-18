d50deca6ab813f81003cb08c20bdd1396e79e61e
j222su <patrickstar0910@gmail.com>
2020-02-18 15:35:20 +0900

    메뉴 사진 업로드시 미리보기 구현 중

--- seller/js/seller_register.js
+++ seller/js/seller_register.js
@@ -7,20 +7,32 @@ $(document).ready(function(){
   ;
 
   //메뉴 추가 버튼 클릭 시 테이블 행도 같이 추가 된다.
+  var count = 0;
   $button_add.click(function(){
-    $div_add_input.append ('<input class="input_info" id="input_add_menu" type="text" name="" value=""> <button class="button_circle_del" type="button" name="button">-</button> </br>');
-    $table_menu.append('<tr ><td>갈릭페뇨파스타 </td><td>' + "22,000" + '</td><td>' + '<input type="file" name="" value="">' + '</td></tr>');
+    $div_add_input.append ('<input class="input_info" id="input_add_menu" type="text" name="menu" value=""> <button class="button_circle_del" type="button" name="button">-</button> </br>');
+    $table_menu.append('<tr ><td>'+$('input[name=menu]').val()+'</td><td>' + '<input type="number" name="menu" value="">' + '</td><td>' + '<input type="file" name="profile_pt" id="profile_pt" onchange="previewImage(this,"View_area")">' + '</td></tr>');
+    count++;
 
     $('.button_circle_del').click(function() {
       //메뉴 입력칸 삭제
-      $(this).prev().remove();
-      $(this).next().remove();
-      $(this).remove();
+      $(this).prev().remove(); //input text지우기
+      $(this).next().remove(); //</br> 지우기
+      $(this).remove(); //버튼 지우기
 
-      //메뉴 테이블(메뉴이름, 가격, 사진)
+
       $("tr:last").remove();
+      // $('tbody>td>input[name:menu]').remove();
+      // $("#table_menu > tbody:last > tr:last").remove();
+
+
+      count--;
+
+
     }); // end of button_circle_del
   }); // end of button_add.click
+
+
+
 
   //브레이크 타임 있음 클릭 시 시간 선택 보이기
   $('input[name="break_time"]').click(function() {
