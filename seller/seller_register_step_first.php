b698f7fc2b689c3b5718fa5816275400d7cb43a4
j222su <patrickstar0910@gmail.com>
2020-02-19 21:47:52 +0900

    판매자 등록 지도 위치 재조정, 로고 클릭시 메인 페이지로 이동

--- seller/seller_register_step_first.php
+++ seller/seller_register_step_first.php
@@ -1,6 +1,6 @@
 
       <div class="div_step">
-        <img class="img_echelin_logo" src="../seller/image/cheese.png" alt="">
+        <img class="img_echelin_logo" src="../seller/image/cheese.png" alt="" onclick="location.href='../'">
         <span class="span_step_info">1단계 : 기본 사항을 입력해주세요.</span>
       </div>
 
@@ -11,7 +11,7 @@
             <span class="span_hello">님 안녕하세요!</span>
             <span class="span_register_info">가게 등록을 시작해볼까요?</span>
             <div class="div_form">
-              <form class="" name="form_seller_register_step_first" action="./seller_register_step_first2.php" method="post">
+              <form class="" name="form_seller_register_step_first" action="./seller_register_step_second.php" method="post">
                 <div class="div_except_button">
                   <ul>
                     <li>등록하시려는 가게 이름을 적어주세요.</li>
--- seller/seller_register_step_second.php
+++ seller/seller_register_step_second.php
@@ -17,8 +17,46 @@ $input_store_name = $_POST["input_store_name"];
     <link rel="stylesheet" href="/echelin/common/css/common.css">
     <link rel="stylesheet" href="/echelin/common/css/search.css">
     <script type="text/javascript">
+      function showMap(address) {
+        var mapContainer = document.getElementById('div_map'), // 지도를 표시할 div
+            mapOption = {
+                center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+                level: 3 // 지도의 확대 레벨
+            };
 
+        // 지도를 생성합니다
+        var map = new kakao.maps.Map(mapContainer, mapOption);
 
+        // 주소-좌표 변환 객체를 생성합니다
+        var geocoder = new kakao.maps.services.Geocoder();
+
+        // 주소로 좌표를 검색합니다
+        geocoder.addressSearch(address, function(result, status) {
+
+            // 정상적으로 검색이 완료됐으면
+             if (status === kakao.maps.services.Status.OK) {
+
+                var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+                // 결과값으로 받은 위치를 마커로 표시합니다
+                var marker = new kakao.maps.Marker({
+                    map: map,
+                    position: coords
+                });
+
+                // 인포윈도우로 장소에 대한 설명을 표시합니다
+                var infowindow = new kakao.maps.InfoWindow({
+                    content: '<div style="width:150px;text-align:center;padding:6px 0;"><? echo $input_store_name;?></div>'
+                });
+                infowindow.open(map, marker);
+
+                // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+                map.setCenter(coords);
+
+                mapContainer.css('display', ($(this).val() === 'true') ? 'block' : 'none');
+            }
+        });
+      }
     </script>
   </head>
   <body>
@@ -30,11 +68,16 @@ $input_store_name = $_POST["input_store_name"];
 
     <section>
       <div class="div_step">
-        <img class="img_echelin_logo" src="../seller/image/cheese.png" alt="">
+        <img class="img_echelin_logo" src="../seller/image/cheese.png" alt="" onclick="location.href='../'">
         <span class="span_step_info">2단계 : 정확한 식당 위치를 등록해주세요.</span>
       </div>
 
       <progress value="33.2" max="100"></progress>
+
+      <div class="div_outside">
+        <div id="div_map"></div> <!-- 지도 담는 div -->
+      </div>
+
 
       <div class="div_register_shape">
         <div class="div_register_inner_shape">
@@ -49,13 +92,12 @@ $input_store_name = $_POST["input_store_name"];
                   <button id="button_find_postcode" type="button" name="button" onclick="execDaumPostcode()">우편번호 찾기</button>
                   </br></br>
                   <input class="input_info" id="input_address"type="text" placeholder=" 주소">
-                  <!-- <button class="button_show_map" type="button" name="button" onclick="showMap()">지도보기</button> -->
                   </br></br>
                   <input class="input_info" id="input_extraAddress" type="text" placeholder=" 참고항목">
                   </br></br>
                   <input class="input_info" id="input_detailAddress" type="text" placeholder=" 예) 상가 2층에 위치해있습니다.">
                   </br></br>
-                  <div id="div_map" style="width:100%;height:350px;"></div> <!-- 지도 담는 div -->
+
                 </div> <!-- div_except_button -->
                 <div class="div_prv_next_button">
                   <button class="button_next" type="button" name="button" onclick="location.href='./seller_register_step_third.php'">다음</button>
