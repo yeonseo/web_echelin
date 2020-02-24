5fb7a8cc54462793a3712a4d6aca2e9c5669b9e6
yeonseo <yulga_nara@naver.com>
2020-02-24 15:18:41 +0900

    user 폴더 안에 마이페이지 추가

---
+++ user/user_myinfo_main.php
@@ -0,0 +1,81 @@
+<div class="my_info_content">
+    <div class="left_menu">
+
+        <div class="my_info_profile">
+            <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/page_form/my_info/index_my_info.php"><img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/image/pengsu1.jpg"></a>
+        </div>
+
+        <!-- 순서대로쭉쭉 -->
+        <ul>
+            <li class="<?= COMMON::$css_sub_menu; ?>"><a href="#">내 정보관리</a> </li>
+            <li class="<?= COMMON::$css_sub_menu; ?>"><a href="#">기타등등관리</a> </li>
+            <li class="<?= COMMON::$css_sub_menu; ?>"><a href="#">기타등등관리</a> </li>
+            <li class="<?= COMMON::$css_sub_menu; ?>"><a href="#">기타등등관리</a> </li>
+            <li class="<?= COMMON::$css_sub_menu; ?>"><a href="#">기타등등관리</a> </li>
+        </ul>
+    </div>
+    <div class="right_content">
+
+
+        <div class="<?= COMMON::$css_card_menu_row; ?>">
+            <button class="<?= COMMON::$css_card_menu_btn; ?>" type="button" onclick="location.href='http\://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php'">
+                <div class="<?= COMMON::$css_card_menu_btn_icon; ?>">
+                    <i class="fas fa-user-cog"></i>
+                </div>
+                <div class="<?= COMMON::$css_card_menu_btn_name; ?>">ICON NAME</div>
+                <div class="<?= COMMON::$css_card_menu_btn_disc; ?>">설명충 설명중.. 설명충충충추...웅...</div>
+            </button>
+
+            <button class="<?= COMMON::$css_card_menu_btn; ?>" type="button" onclick="location.href='http\://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php'">
+                <div class="<?= COMMON::$css_card_menu_btn_icon; ?>">
+                    <i class="far fa-id-card"></i>
+                </div>
+                <div class="<?= COMMON::$css_card_menu_btn_name; ?>">ICON NAME</div>
+                <div class="<?= COMMON::$css_card_menu_btn_disc; ?>">설명충 설명중.. 설명충충충추...웅...</div>
+            </button>
+
+        </div> <!-- end of css_card_menu_row -->
+
+
+
+        <div class="<?= COMMON::$css_card_menu_row; ?>">
+            <button class="<?= COMMON::$css_card_menu_btn; ?>" type="button" onclick="location.href='http\://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php'">
+                <div class="<?= COMMON::$css_card_menu_btn_icon; ?>">
+                    <i class="fas fa-utensils"></i>
+                </div>
+                <div class="<?= COMMON::$css_card_menu_btn_name; ?>">ICON NAME</div>
+                <div class="<?= COMMON::$css_card_menu_btn_disc; ?>">설명충 설명중.. 설명충충충추...웅...</div>
+            </button>
+
+            <button class="<?= COMMON::$css_card_menu_btn; ?>" type="button" onclick="location.href='http\://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php'">
+                <div class="<?= COMMON::$css_card_menu_btn_icon; ?>">
+                    <i class="far fa-id-card"></i>
+                </div>
+                <div class="<?= COMMON::$css_card_menu_btn_name; ?>">ICON NAME</div>
+                <div class="<?= COMMON::$css_card_menu_btn_disc; ?>">설명충 설명중.. 설명충충충추...웅...</div>
+            </button>
+
+        </div> <!-- end of css_card_menu_row -->
+
+
+
+        <div class="<?= COMMON::$css_card_menu_row; ?>">
+            <button class="<?= COMMON::$css_card_menu_btn; ?>" type="button" onclick="location.href='http\://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php'">
+                <div class="<?= COMMON::$css_card_menu_btn_icon; ?>">
+                    <i class="fas fa-clipboard-list"></i>
+                </div>
+                <div class="<?= COMMON::$css_card_menu_btn_name; ?>">ICON NAME</div>
+                <div class="<?= COMMON::$css_card_menu_btn_disc; ?>">설명충 설명중.. 설명충충충추...웅...</div>
+            </button>
+
+            <button class="<?= COMMON::$css_card_menu_btn; ?>" type="button" onclick="location.href='http\://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php'">
+                <div class="<?= COMMON::$css_card_menu_btn_icon; ?>">
+                    <i class="far fa-question-circle"></i>
+                </div>
+                <div class="<?= COMMON::$css_card_menu_btn_name; ?>">ICON NAME</div>
+                <div class="<?= COMMON::$css_card_menu_btn_disc; ?>">설명충 설명중.. 설명충충충추...웅...</div>
+            </button>
+
+        </div> <!-- end of css_card_menu_row -->
+
+    </div><!-- end of right_content -->
