94fb2089da88569a20a069b74f2cf714fa621b61
yeonseo <yulga_nara@naver.com>
2020-02-24 15:32:37 +0900

    작은헤더에서 유저, 판메자, 관리자 페이지 진입하도록 수정 중

--- common/page_form/small_header/header_small.php
+++ common/page_form/small_header/header_small.php
@@ -14,35 +14,40 @@ else $userpoint = "";
 
 <div class="search_header">
 
-    <div class="search_profile">
-        <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/page_form/my_info/index_my_info.php"><img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/image/pengsu1.jpg"></a>
-    </div>
+  <div class="search_profile">
+    <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/page_form/my_info/index_my_info.php"><img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/image/pengsu1.jpg"></a>
+  </div>
 
-    <div class="search_top">
-        <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php"><img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/image/restaurant.png"></a>
+  <div class="search_top">
+    <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php"><img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/image/restaurant.png"></a>
 
-        <ul class="header_main_menu">
-            <!-- 공통 -->
-            <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/search/index_search.php">검색</a></li>
+    <ul class="header_main_menu">
+      <!-- 공통 -->
+      <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/search/index_search.php">검색</a></li>
 
-            <!-- 로그인 성공시 나타나야할 메뉴 -->
-            <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">저장목록</a></li>
-            <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">일정</a></li>
-            <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">메세지</a></li>
+      <!-- 로그인 성공시 나타나야할 메뉴 -->
+      <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">저장목록</a></li>
+      <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">일정</a></li>
+      <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">메세지</a></li>
 
-            <!-- 로그인 전 나타나야할 메뉴 -->
-            <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">로그인</a></li>
-            <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">회원가입</a></li>
+      <!-- 로그인 전 나타나야할 메뉴 -->
+      <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">로그인</a></li>
+      <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">회원가입</a></li>
 
-            <!-- 공통 -->
-            <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">도움말</a></li>
-        </ul>
+      <!-- 각각 보여질 부분 -->
+      <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/user/#.php">유저</a></li>
+      <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">셀러</a></li>
+      <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">관리자</a></li>
 
-    </div>
-    <form class="" action="index_search.php" method="post">
-      <input class="search_input" type="text" placeholder=" &nbsp&nbsp검색어를 입력해주세요.">
-      <button class="search_result_btn">검색</button>
-      <button type="button" name="button" class="search_result_btn" id="keyword_btn">&nbsp키워드 ▼</button>
-   </form>
+      <!-- 공통 -->
+      <li class="<?= COMMON::$css_header_menu; ?>"><a href="#">도움말</a></li>
+    </ul>
+
+  </div>
+  <form class="" action="index_search.php" method="post">
+    <input class="search_input" type="text" placeholder=" &nbsp&nbsp검색어를 입력해주세요.">
+    <button class="search_result_btn">검색</button>
+    <button type="button" name="button" class="search_result_btn" id="keyword_btn">&nbsp키워드 ▼</button>
+  </form>
 
 </div>
