206088f8c196ace50301a78f4b7363b44e32de0e
hadongwoon <hadong302@naver.com>
2020-02-25 18:14:22 +0900

    식당 reservation main에 information.php추가

---
+++ reservation/information.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="./css/information.css">
+</head>
+<body>
+      <div class="div_img_box">
+        <div class="div_img">
+          <div class="gallery-item"> <img src="img/gallery/01.jpg" class="img-responsive" alt=""></div>
+        </div>
+        <div class="div_img">
+          <div class="gallery-item"> <img src="img/gallery/02.jpg" class="img-responsive" alt=""></div>
+        </div>
+        <div class="div_img">
+          <div class="gallery-item"> <img src="img/gallery/03.jpg" class="img-responsive" alt=""></div>
+        </div>
+        <div class="div_img">
+          <div class="gallery-item"> <img src="img/gallery/04.jpg" class="img-responsive" alt=""></div>
+        </div>
+      </div>
+
+    <div class="div_title">
+      <div class=".div_main_img">
+          <img src="img/chef.jpg" alt="...">
+      </div>
+
+      <div class="div_text_box">
+        <div class="section-title">
+          <h2>제목제목제목제목제목제목제목제목제목제목</h2>
+        </div>
+        <p>설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명설명</p>
+
+      </div>
+    </div>
+
+
+
+
+
+</body>
+</html>
