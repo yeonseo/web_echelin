16758b53d232845f6a83c50725911a3b566af144
yeonseo <yulga_nara@naver.com>
2020-02-25 14:43:45 +0900

    메인에서 상점 클릭 시, 나타나는 상점 상세 페이지 구현

--- restaurants/restaurants_main.php
+++ restaurants/restaurants_main.php
@@ -18,116 +18,103 @@
         </div>
     </div>
 
-    <div class="restaurants_main_upper_btn_box">
+    <div class="restaurants_upper_btn_box">
         <button><i class="fas fa-heart"></i> &nbsp; List </button>
         <button><i class="fas fa-share-alt"></i> &nbsp; Share</button>
     </div>
-    <div class="restaurants_main_bottom_btn_box">
+    <div class="restaurants_bottom_btn_box">
         <button><i class="fas fa-heart"></i> &nbsp; List </button>
     </div>
 
-    <div class="right_content">
 
 
+    <div class="restaurants_main_content_box">
 
-        <?php
-        function getJsonDataMakeArticle()
-        {
-            // 디비에서 상점 데이터를 가지고 와서 만들어줄것
-            // 일단 임시값
-            $upso_nm = '가게이름이야아앙';
-            $upso_keyword = '이거슨 키워드 리스트가 될 예저어어어엉ㅍ';
-            $upso_description = '가게 설며엉어어어어엎';
-            $upso_facilities = '이거슨 편의시설 리스트가 될 예저어어어엉ㅍ';
 
-            // 다차원 배열 반복처리 (필요시 사용)
-            ini_set('memory_limit', '-1');
+        <div class="restaurants_center_content">
 
-            if (json_last_error() > 0) {
-                echo json_last_error_msg() . PHP_EOL;
+            <div class="restaurants_main_content_right_btn_box">
+                <button><i class="fas fa-utensils"></i> &nbsp; 예약하러 가기 </button>
+            </div>
+
+            <?php
+
+            if (isset($_GET['seller_num'])) {
+                $seller_num = $_GET['seller_num'];
+            } else {
+                // echo "console.log('레스토랑 주소가 이상한데에~')";
             }
 
-            //print_r($result_json);
-            //print_r($sub_json_object_array); // 배열 요소를 출력해준다.
+            $seller_num = 1;
+            function getJsonDataMakeArticle($con, $dbname, $seller_num)
+            {
+
+                //restaurants 테이블에서 값들고옴
+                $seller_num = 1;
+
+                $sql = "select * from " . $dbname . ".seller where seller_num=" . $seller_num;
+                $result = $con->query($sql);
+                if ($result === FALSE) {
+                    die('DB seller Connect Error: ' . mysqli_error($con));
+                }
+
+                $result_restaurant = mysqli_fetch_array($result);
+
+                // 디비에서 상점 데이터를 가지고 와서 만들어줄것
+                // 일단 임시값
+                $upso_nm = $result_restaurant['store_name'];
+                $upso_keyword = '이거슨 키워드';
+                $upso_description = $result_restaurant['introduction'];
+                $upso_facilities = explode(',', $result_restaurant['convenient_facilities']);
+
+                // 다차원 배열 반복처리 (필요시 사용)
+                ini_set('memory_limit', '-1');
+
+                if (json_last_error() > 0) {
+                    echo json_last_error_msg() . PHP_EOL;
+                }
+
+                //print_r($result_json);
+                //print_r($sub_json_object_array); // 배열 요소를 출력해준다.
 
 
-            echo "<div class=" . COMMON::$css_article_content_box . ">";
+                echo "<div class=" . COMMON::$css_article_content_box . ">";
 
-            echo "<div class=" . COMMON::$css_article_content_title . ">";
-            echo "<h1>" . $upso_nm . "</h1>";
+                echo "<div class=" . COMMON::$css_article_content_title . ">";
+                echo "<h1>" . $upso_nm . "</h1>";
 
-            echo "<ul>";
-            for ($i = 0; $i < 3; $i++) {
-                echo  "<li>" . $upso_keyword . "</li>";
-            } //end of for
-            echo "</ul>";
-            echo "</div>";
+                echo "<ul class=restaurant_keyword_list>";
+                for ($i = 0; $i < count($upso_facilities); $i++) {
+                    echo  "<li><i class=\"fas fa-hashtag\"></i>" . $upso_keyword . "</<i></li>";
+                } //end of for
+                echo "</ul>";
+                echo "</div>";
 
-            echo "<div class=" . COMMON::$css_article_content_sub_title . ">";
-            echo "<p>" . $upso_description . "</p>";
-            echo "<button>식당에 연락하기</button>";
-            echo "</div>";
+                echo "<div class=" . COMMON::$css_article_content_sub_title . ">";
+                echo "<p>" . $upso_description . "</p>";
+                echo "<a href=\"#\">식당에 문의하기</a>";
+                echo "</div>";
 
-            echo "<div class=" . COMMON::$css_card_menu_btn_disc . ">";
-            echo "<h3>편의시설</h3>";
-            echo "<ul>";
-            for ($i = 0; $i < 4; $i++) {
-                echo  "<li>" . $upso_facilities . "</li>";
-            } //end of for
-            echo "<button>편의시설 모두보기</button>";
-            echo "</ul>";
+                echo "<div class=" . COMMON::$css_card_menu_btn_disc . ">";
+                echo "<h3>편의시설</h3>";
+                echo "<ul class=restaurant_facilities_list>";
+                for ($i = 0; $i < count($upso_facilities); $i++) {
+                    echo  "<li><i class=\"fas fa-hashtag\"></i>" . $upso_facilities[$i] . "</<i></li>";
+                } //end of for
+                echo "<a href=\"#\">편의시설 모두보기</a>";
+                echo "</ul>";
 
-            echo "</br>무권아 코맨드 집어넣어죠오오오오ㅓㅏㅁㅊ핀엎 ㅗ민어푸니</br>";
-            echo "</div> ";
+                echo "</br>무권아 코맨드 집어넣어죠오오오오ㅓㅏㅁㅊ핀엎 ㅗ민어푸니</br>";
+                echo "</i> ";
 
-            echo "</div> <!-- end of css_article_content_box -->";
-        }
+                echo "</div> <!-- end of css_article_content_box -->";
+            }
 
-        getJsonDataMakeArticle();
-        ?>
+            getJsonDataMakeArticle($con, $dbname, $seller_num);
+            ?>
 
+        </div><!-- end of restaurants_center_content -->
 
-        <div class="<?= COMMON::$css_card_menu_row; ?>">
+    </div><!-- end of restaurants_main_content_box -->
 
-            <button class="<?= COMMON::$css_card_menu_btn; ?>" type="button" onclick="location.href='http\://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php'">
-                <div class="<?= COMMON::$css_card_menu_btn_icon; ?>">
-                    <i class="far fa-id-card"></i>
-                </div>
-                <div class="<?= COMMON::$css_card_menu_btn_name; ?>">정보변경(업뎃예정)</div>
-                <div class="<?= COMMON::$css_card_menu_btn_disc; ?>">설명충 설명중.. 설명충충충추...웅...</div>
-            </button>
-
-            <button class="<?= COMMON::$css_card_menu_btn; ?>" type="button" onclick="location.href='http\://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/user/??.php'">
-                <div class="<?= COMMON::$css_card_menu_btn_icon; ?>">
-                    <i class="fas fa-clipboard-list"></i>
-                </div>
-                <div class="<?= COMMON::$css_card_menu_btn_name; ?>">예약내역 확인</div>
-                <div class="<?= COMMON::$css_card_menu_btn_disc; ?>">돈을벌자 돈을벌자 뜐뜐!</div>
-            </button>
-
-        </div> <!-- end of css_card_menu_row -->
-
-
-
-        <div class="<?= COMMON::$css_card_menu_row; ?>">
-            <button class="<?= COMMON::$css_card_menu_btn; ?>" type="button" onclick="location.href='http\://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php'">
-                <div class="<?= COMMON::$css_card_menu_btn_icon; ?>">
-                    <i class="fas fa-utensils"></i>
-                </div>
-                <div class="<?= COMMON::$css_card_menu_btn_name; ?>">ICON NAME</div>
-                <div class="<?= COMMON::$css_card_menu_btn_disc; ?>">설명충 설명중.. 설명충충충추...웅...</div>
-            </button>
-
-            <button class="<?= COMMON::$css_card_menu_btn; ?>" type="button" onclick="location.href='http\://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php'">
-                <div class="<?= COMMON::$css_card_menu_btn_icon; ?>">
-                    <i class="far fa-id-card"></i>
-                </div>
-                <div class="<?= COMMON::$css_card_menu_btn_name; ?>">ICON NAME</div>
-                <div class="<?= COMMON::$css_card_menu_btn_disc; ?>">설명충 설명중.. 설명충충충추...웅...</div>
-            </button>
-
-        </div> <!-- end of css_card_menu_row -->
-
-
-
-    </div><!-- end of right_content -->
+</div><!-- end of restaurants_content -->
