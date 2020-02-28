557dee3a396e5ed4c8d051414cf6f0b383524425
youyoungsam <tls1577@nate.com>
2020-02-28 16:58:50 +0900

    검색기능수정중

--- search/js/search.js
+++ search/js/search.js
@@ -2,10 +2,14 @@ $(document).ready(function(){
   var $keyword_btn=$("#keyword_btn");
   var $keyword_header=$("#keyword_header");
   var count=0;
-  //음식종류버튼변수
+  //식당종류버튼변수
   var $food_type_btn=$("#food_type_btn");
-  var $food_type_form=$(".food_type_form");
+  var $food_type_form=$("#food_type_form");
   var f_count=0;
+  var $food_type_select=$(".food_type_select");
+  var selected_res=false;
+  var select_restaurant="";
+  var select_restaurant_name="";
   // 필터버튼클릭시
   $keyword_btn.click(function(){
     if(count==0){
@@ -16,17 +20,29 @@ $(document).ready(function(){
       count=0;
     }
   });//필터버튼
-  //음식종류버튼클릭시
+  //식당 종류버튼클릭시
   $food_type_btn.click(function(){
-    if(f_count=0){
-      $food_type_form.show("slow");
+    if(f_count==0){
+      $food_type_form.css("display","block");
       f_count++;
     }else{
-      $food_type_form.hide("slow");
+      $food_type_form.css("display","none");
       f_count=0;
     }
   });
+  //식당선텍시
+  $food_type_select.click(function(){
+    if(selected_res==false){
+      $(this).css("color","red");
+      selected_res=true;
+      //검색하는순간넘겨줄식당종류변수
+      select_restaurant=$(this).attr('id');
+      select_restaurant_name=$(this).text();
+      $food_type_btn.text(select_restaurant_name);
+    }else{
 
-  
+    }
+  });
 
-});
+
+});//ready
--- search/main_search.php
+++ search/main_search.php
@@ -1,27 +1,28 @@
 
-
 <!-- 키워드테이블에서가져와서뿌리기 -->
 <div id="keyword_header" style=" display:none; position:relative;">
   <ul class="keywords">
-    <li id="food_type_btn" onclick="food_type_btn();">음식종류</li>
+    <li id="food_type_btn">식당종류</li>
     <li id="keyword_search_btn">#키워드로검색</li>
     <li id="gps_btn">현재위치에서검색</li>
   </ul>
 </div>
-<div class="food_type_form">
+<div id="food_type_form">
   <ul>
-    <li>한식당</li>
-    <li>일식당</li>
-    <li>중식당</li>
-    <li>이태리식당</li>
-    <li>고깃집</li>
-    <li>횟집</li>
-    <li>기사식당</li>
-    <li>카페</li>
-    <li>뷔페</li>
-    <li>분식집</li>
-    <li>제과점</li>
-    <li>술집</li>
+    <li class="food_type_select" id="korean_res">한식당</li>
+    <li class="food_type_select" id="japaness_res">일식당</li>
+    <li class="food_type_select" id="chiness_res">중식당</li>
+    <li class="food_type_select" id="italy_res">이태리식당</li>
+    <li class="food_type_select" id="meat_res">고깃집</li>
+    <li class="food_type_select" id="fish_res">횟집</li>
+    <li class="food_type_select" id="driver_res">기사식당</li>
+    <li class="food_type_select" id="cafe">카페</li>
+    <li class="food_type_select" id="buffet">뷔페</li>
+    <li class="food_type_select" id="snack_shop">분식집</li>
+    <li class="food_type_select" id="bakery">제과점</li>
+    <li class="food_type_select" id="bar">술집</li>
+    <li id="food_type_select_save">저장</li>
+    <li id="food_type_select_delete">지우기</li>
   </ul>
 </div>
 <!-- 많이검색된키워드db에서가져와서정렬 -->
@@ -61,7 +62,9 @@
     <span class="search_title">테스트 &nbsp;&nbsp;:::&nbsp;&nbsp; "#" 에 대한 모든 검색결과 입니다. 무한 스크롤</span>
 
     <div class="search_member">
-        <?php  ?>
+        <?php
+
+        ?>
         <div class="all_content_first"></div>
         <div class="all_content"></div>
         <div class="all_content"></div>
