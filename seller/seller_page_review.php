bdaea048c369083372c904c02c1fe1a0b0c0347f
yeonseo <yulga_nara@naver.com>
2020-03-03 01:30:47 +0900

    사이트 메뉴 묶어서 관리하도록 변경 및 셀러 메인 페이지 버튼 수정

--- seller/seller_page_review.php
+++ seller/seller_page_review.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+
 <head>
   <meta charset="utf-8">
   <?php include $_SERVER['DOCUMENT_ROOT'] . "/echelin/common/common_class_value.php"; ?>
@@ -13,36 +14,34 @@
   <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 
   <script type="text/javascript">
-
-    function test(){
+    function test() {
 
       var html = "<div class='content_yes'>";
 
-            html += "<div class='yes_top'>";
-            
-            html += "<div class='comment_profile_img'>";
-            html += "<a href='#'><img src='http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/image/pengsu1.jpg'></a>"
-            html += "</div>";
+      html += "<div class='yes_top'>";
 
-            html += "<span><a href='#'>유저 이름</a> &nbsp·&nbsp 등록 시간</span>";
-            html += "<div class='div_chu'>";
-            html += "<div id='like_count'><img src='./image/like.png'> &nbsp;0</div>";
-            html += "<div id='dislike_count'><img src='./image/dislike.png'> &nbsp;0</div>";
-            html += "</div>";
+      html += "<div class='comment_profile_img'>";
+      html += "<a href='#'><img src='http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/image/pengsu1.jpg'></a>"
+      html += "</div>";
 
-            html += "</div>";
+      html += "<span><a href='#'>유저 이름</a> &nbsp·&nbsp 등록 시간</span>";
+      html += "<div class='div_chu'>";
+      html += "<div id='like_count'><img src='./image/like.png'> &nbsp;0</div>";
+      html += "<div id='dislike_count'><img src='./image/dislike.png'> &nbsp;0</div>";
+      html += "</div>";
 
-            html += "<div class='yes_main'>";
-            html += "후기 내용 공간";
-            html += "</div>";
+      html += "</div>";
 
-        html += "</div>";
+      html += "<div class='yes_main'>";
+      html += "후기 내용 공간";
+      html += "</div>";
+
+      html += "</div>";
 
       $(".content_main .content_no").remove();
       $(".content_main").append(html);
 
     }
-
   </script>
 
 </head>
@@ -57,59 +56,44 @@
 
     <div class="my_info_content">
 
-        <div class="left_menu">
+      <div class="left_menu">
+        <!-- 사이드 메뉴 -->
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/echelin/seller/seller_side_left_menu.php"; ?>
+      </div>
 
-            <div class="my_info_profile">
-                <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/page_form/my_info/index_my_info.php"><img src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/common/image/pengsu1.jpg"></a>
-            </div>
+      <div class="right_content">
 
-            <!-- 순서대로쭉쭉 -->
-            <ul>
-                <li class="<?= COMMON::$css_sub_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php">셀러 정보변경(업뎃예정)</a> </li>
-                <li class="<?= COMMON::$css_sub_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/seller/common_page.php">ex가게 등록/변경하기(참고용)</a> </li>
-                <li class="<?= COMMON::$css_sub_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/seller/??.php">매출보기</a> </li>
-                <li class="<?= COMMON::$css_sub_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/seller/common_page.php">예약관리페이지</a> </li>
-                <li class="<?= COMMON::$css_sub_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/seller/common_page.php">메세지</a> </li>
-                <li class="<?= COMMON::$css_sub_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/seller/seller_page_review.php">고객이 남긴 후기</a> </li>
-                <li class="<?= COMMON::$css_sub_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/seller/common_page.php">문의게시판(관리자랑)</a> </li>
-                <li class="<?= COMMON::$css_sub_menu; ?>"><a href="#">리스트를 넣어요~</a> </li>
-                <li class="<?= COMMON::$css_sub_menu; ?>"><a href="#">리스트를 넣어요~</a> </li>
-            </ul>
-        </div>
+        <div class="container">
 
-        <div class="right_content">
-
-      <div class="container">
-
-        <div class="my_comment_title">
-          <span class="comment_title_sub">마이 페이지 &nbsp&nbsp > &nbsp&nbsp 고객이 남긴 후기</span><br><br>
-          <span class="comment_title_main">우리 가게 후기</span>
-        </div>
+          <div class="my_comment_title">
+            <span class="comment_title_sub">마이 페이지 &nbsp&nbsp > &nbsp&nbsp 고객이 남긴 후기</span><br><br>
+            <span class="comment_title_main">우리 가게 후기</span>
+          </div>
 
           <div class="tab_container">
-              <div class="tab_content">
+            <div class="tab_content">
 
-                  <ul>
-                      <li>
+              <ul>
+                <li>
 
-                        <div class="tab_content_first">
-                          <div class="content_title"><span>내가 받은 후기</span></div>
+                  <div class="tab_content_first">
+                    <div class="content_title"><span>내가 받은 후기</span></div>
 
-                          <div class="content_main">
-                            <div class="content_no">
-                              <span class="no_span">받은 후기가 없어요.</span>
-                            </div>
+                    <div class="content_main">
+                      <div class="content_no">
+                        <span class="no_span">받은 후기가 없어요.</span>
+                      </div>
 
-                          </div>
-                        </div>
-                      </li>
+                    </div>
+                  </div>
+                </li>
 
-                  </ul>
-
-              </div>
-              <!-- #tab1 -->
+              </ul>
 
             </div>
+            <!-- #tab1 -->
+
+          </div>
           <!-- .tab_container -->
 
           <button type="button" name="button" onclick="test()">테스트</button>
