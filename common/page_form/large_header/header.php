fa43037f73e00bb0bd68989491547959e2ab44b8
yeonseo <yulga_nara@naver.com>
2020-03-02 21:46:32 +0900

    섹션부분 사소한 부분 수정

--- common/page_form/large_header/header.php
+++ common/page_form/large_header/header.php
@@ -20,84 +20,68 @@ else $userpoint = "";
 ?>
 
 <div class="main_header">
-    <div class="back_cover">
-        <div class="main_head_top">
-            <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php"><img src="./common/image/restaurant.png"></a>
-            <ul class="header_main_menu">
-      <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/search/index_search.php">검색</a></li>
-      <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/help_center/help_center_main.php">도움말</a></li>
+  <div class="back_cover">
+    <div class="main_head_top">
+      <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/index.php"><img src="./common/image/restaurant.png"></a>
+      <ul class="header_main_menu">
 
 
-               
-      <?
-      if(! $useremail){
-      ?>
-      <!-- 로그인 전 나타나야할 메뉴 -->
-      <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/user/user_login_select.php">로그인</a></li>
-      <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/user/user_join_main.php">회원가입</a></li>
-      <?
-      }else{
-        $log = $useremail."(".$username."님] [Level:".$userlevel."Point:".$userpoint."]";
-      ?>
-      <li class="<?= COMMON::$css_header_menu; ?>"> <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/user/user_myinfo_index.php"><?=$log?></a></li>
-
-      <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/user/user_logout.php">로그아웃</a></li>
-
-
-
-
-      <?
+        <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/search/index_search.php">검색</a></li>
+        <?
+        if (!$useremail) {
+        ?>
+          <!-- 로그인 전 나타나야할 메뉴 -->
+          <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/user/user_login_select.php">로그인</a></li>
+          <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/user/user_join_main.php">회원가입</a></li>
+        <?
+        } else {
+          // 로그인한 유저의 이름 표시 및 로그아웃 표시
+          $log = $useremail . "/" . $username . "님";
+        ?>
+          <li class="<?= COMMON::$css_header_menu; ?>"> <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/user/user_myinfo_index.php"><?= $log ?></a></li>
+          <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/user/user_logout.php">로그아웃</a></li>
+        <?
         }
-      ?>
+        ?>
 
-      <!-- 사용자 판매자 관리자 화면 나누는 부분 -->
-      <?
-        if($userlevel == 1){
-      ?>
-        <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/user/user_myinfo_index.php">유저</a></li>
-     <?
+        <!-- 판매자 / 관리자 인 경우 보이는 화면 -->
+        <?
+        if ($userlevel == 10) {
+        ?>
+          <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/seller/seller_sellerinfo_index.php">상점관리</a></li>
+        <?
         }
-      ?>
+        ?>
 
-      <?
-        if($userlevel == 10){
-      ?>
-
-
-        <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/seller/seller_sellerinfo_index.php">셀러</a></li>
-      <?
+        <?
+        if ($userlevel == 100) {
+        ?>
+          <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/admin/admin_myinfo_index.php">관리하기</a></li>
+        <?
         }
-      ?>
-
-      <?
-        if($userlevel == 100){
-      ?>
-        <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/admin/admin_myinfo_index.php">관리자</a></li>
-      <?
-        }
-      ?>
-      <!-- 끝 -->
+        ?>
+        <li class="<?= COMMON::$css_header_menu; ?>"><a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/echelin/help_center/help_center_main.php">도움말</a></li>
 
 
-            </ul>
-        </div>
+      </ul>
+    </div>
 
-        <div class="main_header_centers">
-            <a href="#">
-                <div id="center_first"></div>
-            </a>
-            <a href="#">
-                <div id="center_second"></div>
-            </a>
-            <a href="#">
-                <div id="center_third"></div>
-            </a>
-        </div>
+    <div class="main_header_centers">
+      <a href="#">
+        <div id="center_first"></div>
+      </a>
+      <a href="#">
+        <div id="center_second"></div>
+      </a>
+      <a href="#">
+        <div id="center_third"></div>
+      </a>
+    </div>
 
-    </div> 
-    <!--end ::: back_top_cover -->
+  </div>
+  <!--end ::: back_top_cover -->
 
-    
+
 
 </div>
 <!--end ::: header_main -->
--- user/user_login_select.php
+++ user/user_login_select.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -17,79 +18,81 @@
 
     <title>Document</title>
 </head>
+
 <body>
-    
-   
-<header>
+
+
+    <header>
         <?php include $_SERVER['DOCUMENT_ROOT'] . "/echelin/common/page_form/small_header/header_small.php"; ?>
-</header>
-    
+    </header>
+
     <!-- <section>
       
     </section> -->
 
 
     <div class="user_login_form">
-    <div style="margin-top: 0px">
-    
-    <div class="login_border">
-        <div class="login_bottom_border">
-        <div class="sns_login_select">
-                      <?php include "FB.login.php"; ?>
-                      <?php include "naver_login_form.php"; ?>
-                      <?php include "kakao_login.php"; ?>
-                      <?php include "google_login_form.php"; ?>       
-        </div>
- 
-        </div>
+        <div style="margin-top: 0px">
 
-    
-        <div style="margin-top:0%; margin-bottom:5%">
-        <div>
-         <div>
-             <div class="form_center">
-                 <div class="form_font">
-                   
-                 </div>
-             </div>
-         </div>   
-        </div>
-        <div style="margin-top: -5%; margin-bottom: 16px;">
-
-        <div class="before_O">
-            <span class="before_T">
-                
-                <span class="login_form3">또는</span>
-            </span> 
-
-
-            
-        </div>
-        <!-- onsubmit="return check()" -->
-    <form name="myForm" action="user_login_check.php" method="post" >
-        <div style="margin-bottom:5px">
-              <div class="input_form">
-                  <div class="input_form2">
-
-                  <!-- // 이메일 설정하기 -->
-                  <input  class="_14fdu48d" id="signup_email" name="user_Email" placeholder="이메일 주소" type="text" value="" style="text-align: center" onkeyup="signup_check('signup_email')" > 
-                  <input id="input_form25" type="hidden" name="user_sns" > 
-                  <span id="email_text"></span>
-
-                  <!-- // 비밀번호 설정하기 -->
-                  <input  class="_14fdu48d" id="user" name="user_pass" placeholder="비밀번호" type="password" value="" style="text-align: center;" onkeyup="signup_check('user_pass')">
-                  <span id="user_password"></span>
+            <div class="login_border">
+                <div class="login_bottom_border">
+                    <div class="sns_login_select">
+                        <?php include "FB.login.php"; ?>
+                        <?php include "naver_login_form.php"; ?>
+                        <?php include "kakao_login.php"; ?>
+                        <?php include "google_login_form.php"; ?>
                     </div>
-                    <button id="t" type="submit" class="login_data_button" onclick="signup_check();">
-                <span>로그인</span>
-            </button>
-            <div style="margin-top:16px; margin-bottom:16px">
-                    <div class="login_echelin_bottom_form">
-                      이미 echelin 계정이 있으신가요?
-                      <a href="header_small.php" class="login_location_login" aria-busy="false">&nbsp;&nbsp; 로그인</a>
+
+                </div>
+
+
+                <div style="margin-top:0%; margin-bottom:5%">
+                    <div>
+                        <div>
+                            <div class="form_center">
+                                <div class="form_font">
+
+                                </div>
+                            </div>
+                        </div>
                     </div>
-              </div>  
-            </div>
-    
+                    <div style="margin-top: -5%; margin-bottom: 16px;">
+
+                        <div class="before_O">
+                            <span class="before_T">
+
+                                <span class="login_form3">또는</span>
+                            </span>
+
+
+
+                        </div>
+                        <!-- onsubmit="return check()" -->
+                        <form name="myForm" action="user_login_check.php" method="post">
+                            <div style="margin-bottom:5px">
+                                <div class="input_form">
+                                    <div class="input_form2">
+
+                                        <!-- 이메일 입력받기 -->
+                                        <input class="_14fdu48d" id="signup_email" name="user_Email" placeholder="이메일 주소" type="text" value="" style="text-align: center" onkeyup="signup_check('signup_email')">
+                                        <input id="input_form25" type="hidden" name="user_sns">
+                                        <span id="email_text"></span>
+
+                                        <!-- 비밀번호 입력받기 -->
+                                        <input class="_14fdu48d" id="user" name="user_pass" placeholder="비밀번호" type="password" value="" style="text-align: center;" onkeyup="signup_check('user_pass')">
+                                        <span id="user_password"></span>
+                                    </div>
+                                    <button id="t" type="submit" class="login_data_button" onclick="signup_check();">
+                                        <span>로그인</span>
+                                    </button>
+                                    <div style="margin-top:16px; margin-bottom:16px">
+                                        <div class="login_echelin_bottom_form">
+                                            이미 echelin 계정이 있으신가요?
+                                            <a href="header_small.php" class="login_location_login" aria-busy="false">&nbsp;&nbsp; 로그인</a>
+                                        </div>
+                                    </div>
+                                </div>
+
 </body>
+
 </html>
