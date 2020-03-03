4883a21b338b1bc594a71b76aadc0361ce8a74e5
j222su <patrickstar0910@gmail.com>
2020-03-04 03:28:39 +0900

    파일 select 예약현황 select

---
+++ seller/store_pic_update.php
@@ -0,0 +1,74 @@
+<?php
+date_default_timezone_set('Asia/Seoul');
+$upload_dir = './storeImg/';
+$con = mysqli_connect("localhost","root","123456","echelin");
+
+$seller_num=$_GET["seller_num"];
+
+// $file_n	 = $_FILES["file"]["name"];
+print_r($_FILES);
+// var_dump($file_n);
+
+
+for($i=0; $i<5; $i++) {
+$random=mt_rand(1,9999);
+$file_name	 = $_FILES["file"]["name"][$i];
+$file_type	 = $_FILES["file"]["type"][$i];
+$file_tmp_name	 = $_FILES["file"]["tmp_name"][$i];
+$file_error    = $_FILES["file"]["error"][$i];
+$file_size	 = $_FILES["file"]["size"][$i];
+
+
+// print_r($_FILES);
+
+if ($file_name && !$file_error)
+{
+
+  $file = explode(".", $file_name);
+
+  $file_name = $file[0];
+  $file_ext  = $file[1];
+
+  $new_file_name = date("Y_m_d_H_i_s")."_".$random;
+
+  $copied_file_name = $new_file_name.".".$file_ext;
+
+  $uploaded_file = $upload_dir.$copied_file_name;
+
+  if( $file_size  > 6000000 ) {
+      echo("
+      <script>
+      alert('업로드 파일 크기가 지정된 용량(6MB)을 초과합니다!<br>파일 크기를 체크해주세요! ');
+      history.go(-1)
+      </script>
+      ");
+      exit;
+  }
+
+  if (!move_uploaded_file($file_tmp_name, $uploaded_file) )
+  {
+      echo("
+        <script>
+        alert('파일을 지정한 디렉토리에 복사하는데 실패했습니다.');
+        </script>
+      ");
+      exit;
+  }
+}
+else
+{
+  $file_name      = "";
+  $file_type      = "";
+  $copied_file_name = "";
+}
+
+
+$sql = "insert into store_img (num, seller_num, store_name, store_file_name, store_file_type, store_file_copied) ";
+$sql .= "values(null, '$seller_num', (select store_name from seller where seller_num = '$seller_num'), '$file_name', '$file_type', '$copied_file_name')";
+
+mysqli_query($con, $sql);
+var_dump($file_name);
+}
+
+mysqli_close($con);
+?>
