1803788c5d269eeccdf16ab508950af8b7a37f9b
hadongwoon <hadong302@naver.com>
2020-03-04 21:18:14 +0900

    음식 갯수가 음식이름으로 나오는 현상 수정

--- reservation/check.php
+++ reservation/check.php
@@ -22,7 +22,7 @@
   $person=get('person');
   $person_array=explode(',' , $person);
 
-  $insert_menu = $select_menu_title[0].",".$select_menu_title[0];
+  $insert_menu = $select_menu_title[0].",".$select_menu_count[0];
   for($i =1 ; $i < count($select_menu_title) ; $i++){
     $insert_menu.=",".$select_menu_title[$i].",".$select_menu_count[$i];
   }
